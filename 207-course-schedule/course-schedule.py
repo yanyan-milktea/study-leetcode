471189806db307c4f13ea4493eaea2b8b2002a58
yanyan-milktea <imyanchen@gmail.com>
2026-09-16 21:49:02 -0700

    Time: 0 ms (100.00%) | Memory: 20.4 MB (62.76%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+
+        for course, prerequisite in prerequisites:
+            graph[prerequisite].append(course)
+            indegree[course] += 1
+
+        queue = collections.deque()
+
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                queue.append(course)
+
+        completed = 0
+
+        while queue:
+            course = queue.popleft()
+            completed += 1
+
+            for next_course in graph[course]:
+                indegree[next_course] -= 1
+
+                if indegree[next_course] == 0:
+                    queue.append(next_course)
+
+        return completed == numCourses
