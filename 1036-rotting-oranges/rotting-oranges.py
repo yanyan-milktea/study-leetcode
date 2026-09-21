456ad9626d7cd162a33a706a8b85eae35985d59c
yanyan-milktea <imyanchen@gmail.com>
2026-09-21 11:44:39 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (43.22%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,10 +1,11 @@
-from collections import deque
-
 class Solution:
     def orangesRotting(self, grid: list[list[int]]) -> int:
+        # 1. Get rows and columns
         rows = len(grid)
         cols = len(grid[0])
 
+        # 2. Add all rotten oranges to the queue
+        #    and count fresh oranges
         queue = collections.deque()
         fresh = 0
 
@@ -15,13 +16,15 @@ class Solution:
                 elif grid[row][col] == 1:
                     fresh += 1
 
+        # 3. Multi-source BFS
+        #    Each level represents one minute
         minutes = 0
         directions = [(1, 0), (-1, 0), (0, -1), (0, 1)]
-
+        
         while queue and fresh > 0:
-            level_size = len(queue)
+            level = len(queue)
 
-            for _ in range(level_size):
+            for _ in range(level):
                 row, col = queue.popleft()
 
                 for dr, dc in directions:
@@ -31,10 +34,14 @@ class Solution:
                     if ( 0 <= new_row < rows
                         and 0 <= new_col < cols
                         and grid[new_row][new_col] == 1):
-                        grid[new_row][new_col] = 2;
+
+                        grid[new_row][new_col] = 2
                         fresh -= 1
                         queue.append((new_row, new_col))
-            
+                
             minutes += 1
 
+
+        # 4. Return the result
+        #    If fresh oranges remain, return -1
         return minutes if fresh == 0 else -1
